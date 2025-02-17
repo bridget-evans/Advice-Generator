f722eb06c19b96c410a29526fc3c1398713d726b
bridget-evans <169062212+bridget-evans@users.noreply.github.com>
2025-02-17 15:23:18 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const url = 'https://api.adviceslip.com/advice';
+const url = 'https://opentdb.com/api.php?amount=10&category=20&difficulty=easy&type=multiple';
 
 async function getData() {
   const response = await fetch(url);
